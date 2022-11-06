1a2791556af5ab5a5a3f945eeea4331399a177b5
AE <87952618+aelhadee@users.noreply.github.com>
2022-11-06 01:31:05 -0500

    Update start_CAN_RX.sh

--- sandbox/start_CAN_RX.sh
+++ sandbox/start_CAN_RX.sh
@@ -8,3 +8,9 @@ nfdc strategy unset /localhost
 nfdc strategy set /trailerECU/can/data /localhost/nfd/strategy/best-route	
 nfdc face create udp://192.168.10.33
 nfdc route add /trailerECU/can/data udp://192.168.10.33
+
+
+# sender
+nfdc strategy set /trailerECU/can/data /localhost/nfd/strategy/best-route	
+nfdc face create udp://192.168.10.11
+nfdc route add /trailerECU/can/data udp://192.168.10.11
